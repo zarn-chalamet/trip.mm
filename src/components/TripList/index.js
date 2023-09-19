1b41774cdcaea44f8a7662eaa45d894f68cd9d2d
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2023-09-19 11:04:45 +0700

    the last push

--- src/components/TripList/index.js
+++ src/components/TripList/index.js
@@ -4,35 +4,38 @@ import "./index.css";
 
 export default function TripList() {
   let [url, setUrl] = useState("http://localhost:3001/trips");
+  let { data: trips, loading, error } = useFetch(url);
 
-  let { data: trips, loading } = useFetch(url);
   return (
     <div className="container">
-      <div className="flex-container">
-        <h1>Ready to go ?</h1>
-        {loading && <p>loading trips</p>}
-        <div>
-          <button onClick={() => setUrl("http://localhost:3001/trips")}>
-            all
-          </button>
-          <button
-            onClick={() =>
-              setUrl("http://localhost:3001/trips?location=Myanmar")
-            }
-          >
-            Trips in Myanmar
-          </button>
+      {error && <p>{error}</p>}
+      {!error && (
+        <div className="flex-container">
+          <h1>Ready to go ?</h1>
+          {loading && <p>loading trips</p>}
+          <div>
+            <button onClick={() => setUrl("http://localhost:3001/trips")}>
+              all
+            </button>
+            <button
+              onClick={() =>
+                setUrl("http://localhost:3001/trips?location=Myanmar")
+              }
+            >
+              Trips in Myanmar
+            </button>
+          </div>
+          <ul className="trips-list">
+            {trips &&
+              trips.map((trip) => (
+                <li key={trip.id} className="trip">
+                  <h3>{trip.name}</h3>
+                  <p>price - {trip.price} mmk</p>
+                </li>
+              ))}
+          </ul>
         </div>
-        <ul className="trips-list">
-          {trips &&
-            trips.map((trip) => (
-              <li key={trip.id} className="trip">
-                <h3>{trip.name}</h3>
-                <p>price - {trip.price} mmk</p>
-              </li>
-            ))}
-        </ul>
-      </div>
+      )}
     </div>
   );
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,16 +1,34 @@
 import { useEffect, useState } from "react";
+
 function useFetch(url) {
   let [data, setData] = useState(null);
   let [loading, setLoading] = useState(false);
+  let [error, setError] = useState(null);
   useEffect(() => {
+    let abortController = new AbortController();
+    let signal = abortController.signal;
     setLoading(true);
-    fetch(url)
-      .then((respond) => respond.json())
+    fetch(url, { signal })
+      .then((respond) => {
+        if (!respond.ok) {
+          throw Error("something went wrong!");
+        }
+        return respond.json();
+      })
       .then((data) => {
         setData(data);
+        setError(null);
         setLoading(false);
+      })
+      .catch((e) => {
+        setError(e.message);
       });
+
+    //cleanup function
+    return () => {
+      abortController.abort();
+    };
   }, [url]);
-  return { data, loading };
+  return { data, loading, error };
 }
 export default useFetch;
